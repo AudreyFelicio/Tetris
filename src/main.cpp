06c40b1239662d1746c9d5d9b3096bb1f091c4dd
Albert Ariel Widiaatmaja <w.albertariel@bytedance.com>
2023-05-06 01:03:48 +0800

    Adds basic display for grid

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <SFML/Graphics.hpp>
-#include "unit_square.hpp"
+#include <array>
+#include <string>
+#include "grid.hpp"
 
 auto main() -> int {
-  UnitSquare u;
+  std::array<std::array<bool, 4>, 4> x {{
+      {{true, false, false, false}},
+      {{true, false, false, false}},
+      {{true, false, false, false}},
+      {{true, true, true, true}}
+  }};
+  Grid g(x, "#FFFFFF", sf::Vector2f(100.f, 100.f));
+
+  const float LENGTH = 10.0;
+  sf::Vector2f top_left(100.0f, 100.0f);
+
+  sf::RectangleShape square(sf::Vector2f(LENGTH, LENGTH));
+  square.setPosition(sf::Vector2f(top_left.x + LENGTH * 1, top_left.y + LENGTH * 1));
+  square.setFillColor(sf::Color::Blue);
 
   auto window = sf::RenderWindow{ { 1920u, 1080u }, "Tetris" };
   window.setFramerateLimit(144);
@@ -11,8 +26,16 @@ auto main() -> int {
       if (event.type == sf::Event::Closed) {
         window.close();
       }
+      
     }
+    
     window.clear();
+
+    for (auto &i: g.getDisplay()) {
+      window.draw(i);
+    }
+    // window.draw(square);
+
     window.display();
   }
 }
