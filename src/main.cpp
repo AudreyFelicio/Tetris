437064fc5e32c828e384cded4bc024d3955f4a0e
AudreyFelicio <audreyanwar@gmail.com>
2023-05-05 23:54:14 +0800

    feat: Initialize cmake and main

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,16 @@
 #include <SFML/Graphics.hpp>
+#include <iostream>
 
-int main()
-{
-    auto window = sf::RenderWindow{ { 1920u, 1080u }, "CMake SFML Project" };
-    window.setFramerateLimit(144);
-
-    while (window.isOpen())
-    {
-        for (auto event = sf::Event{}; window.pollEvent(event);)
-        {
-            if (event.type == sf::Event::Closed)
-            {
-                window.close();
-            }
-        }
-
-        window.clear();
-        window.display();
+auto main() -> int {
+  auto window = sf::RenderWindow{ { 1920u, 1080u }, "Tetris" };
+  window.setFramerateLimit(144);
+  while (window.isOpen()) {
+    for (auto event = sf::Event{}; window.pollEvent(event); ) {
+      if (event.type == sf::Event::Closed) {
+        window.close();
+      }
     }
+    window.clear();
+    window.display();
+  }
 }
