39e3a156275ea13634f3a4da6795775c071f8465
Albert Ariel Widiaatmaja <w.albertariel@bytedance.com>
2023-05-06 07:25:43 +0800

    Add basic non collision

--- includes/board.hpp
+++ includes/board.hpp
@@ -16,21 +16,114 @@
 class Board {
 using BaseBoard = std::array<std::array<bool, WIDTH>, HEIGHT>;
 public:
+  Board(BaseBoard grid, sf::Color color, sf::Vector2f top_left):
+    grid{grid}, color{color}, top_left{top_left} {
+      background = createBackground();
+    }
+
+  auto spawnRandomPiece() -> void {
+    std::random_device rng;
+    std::uniform_int_distribution<size_t> dist(0, WIDTH - BASE_SIZE);
+    LShape random_piece(sf::Color::Blue, sf::Vector2f(top_left.x + dist(rng) * UNIT_SQUARE_LENGTH, top_left.y));
+    pieces.push_back(random_piece);
+    control_index = pieces.size() - 1;
+  }
+
+  auto draw(sf::RenderWindow& window) const -> void {
+    window.draw(background);
+    for (auto &piece: pieces) {
+      piece.draw(window);
+    }
+  }
+
+  auto moveDown() -> void {
+    if (control_index == -1) {
+      spawnRandomPiece();
+      return;
+    }
+
+    if (control_index < 0 or control_index >= pieces.size()) {
+      return;
+    }
+
+    pieces[control_index].moveDown();
+    if (!validMove()) {
+      pieces[control_index].moveUp();
+      spawnRandomPiece();
+    }
+  }
+
+  auto handleKeyboardInput(sf::Keyboard::Key input) -> void {
+    if (control_index < 0 or control_index >= pieces.size()) {
+      return;
+    }
+
+    switch (input) {
+      case sf::Keyboard::Left:
+        pieces[control_index].moveLeft();
+        if (!validMove()) {
+          pieces[control_index].moveRight();
+        }
+        break;
+      case sf::Keyboard::Right:
+        pieces[control_index].moveRight();
+        if (!validMove()) {
+          pieces[control_index].moveLeft();
+        }
+        break;
+      case sf::Keyboard::Up:
+        pieces[control_index].rotateClockwise();
+        if (!validMove()) {
+          pieces[control_index].rotateCounterclockwise();
+        }
+        break;
+      case sf::Keyboard::Down:
+        pieces[control_index].rotateCounterclockwise();
+        if (!validMove()) {
+          pieces[control_index].rotateClockwise();
+        }
+        break;
+      default:
+        break;
+    }
+  }
+  
 
 protected:
   BaseBoard grid;
   sf::Color color;
   sf::Vector2f top_left;
   std::vector<Piece> pieces;
+  sf::RectangleShape background;
+  int control_index = -1;
   
-  Board(BaseBoard grid, sf::Color color, sf::Vector2f top_left):
-    grid{grid}, color{color}, top_left{top_left} {}
-  
-  auto spawnRandomPiece() -> void {
-    std::random_device rng;
-    std::uniform_int_distribution<size_t> dist(0, WIDTH - BASE_SIZE);
-    LShape random_piece(sf::Color::Blue, sf::Vector2f(dist(rng) * UNIT_SQUARE_LENGTH, 0.0));
-    pieces.push_back(random_piece);
+  auto createBackground() -> sf::RectangleShape {
+    sf::RectangleShape square(sf::Vector2f(UNIT_SQUARE_LENGTH * WIDTH, UNIT_SQUARE_LENGTH * HEIGHT));
+    square.setPosition(top_left);
+    square.setFillColor(color);
+    return square;
+  }
+
+  auto validMove() -> bool {
+    if (control_index < 0 or control_index >= pieces.size()) {
+      return true;
+    }
+
+    if (pieces[control_index].outsideBoundaries(top_left.x, top_left.x + (WIDTH * UNIT_SQUARE_LENGTH), top_left.y, top_left.y + (HEIGHT * UNIT_SQUARE_LENGTH))) {
+      return false;
+    }
+
+    for (int i = 0; i < pieces.size(); ++i) {
+      if (i == control_index) {
+        continue;
+      }
+
+      if (pieces[control_index].collidesWithOtherPiece(pieces[i])) {
+        return false;
+      }
+    }
+
+    return true;
   }
 };
 
--- src/main.cpp
+++ src/main.cpp
@@ -7,20 +7,45 @@
 #include "board.hpp"
 
 auto main() -> int {
-  std::vector<Piece> pieces = {
-    OShape(sf::Color::Blue, {100.0f, 100.0f}),
-    LShape(sf::Color::Blue, {200.0f, 200.0f}),
-    IShape(sf::Color::Blue, {300.0f, 300.0f}),
-    JShape(sf::Color::Blue, {400.0f, 400.0f}),
-    ZShape(sf::Color::Blue, {500.0f, 500.0f}),
-    TShape(sf::Color::Blue, {600.0f, 600.0f}),
-    SShape(sf::Color::Blue, {700.0f, 700.0f})
-  };
-  const float LENGTH = 10.0;
-  sf::Vector2f top_left(100.0f, 100.0f);
-  sf::RectangleShape square(sf::Vector2f(LENGTH, LENGTH));
-  square.setPosition(sf::Vector2f(top_left.x + LENGTH * 1, top_left.y + LENGTH * 1));
-  square.setFillColor(sf::Color::Blue);
+  // std::vector<Piece> pieces = {
+  //   OShape(sf::Color::Blue, {100.0f, 100.0f}),
+  //   LShape(sf::Color::Blue, {200.0f, 200.0f}),
+  //   IShape(sf::Color::Blue, {300.0f, 300.0f}),
+  //   JShape(sf::Color::Blue, {400.0f, 400.0f}),
+  //   ZShape(sf::Color::Blue, {500.0f, 500.0f}),
+  //   TShape(sf::Color::Blue, {600.0f, 600.0f}),
+  //   SShape(sf::Color::Blue, {700.0f, 700.0f})
+  // };
+  // const float LENGTH = 10.0;
+  // sf::Vector2f top_left(100.0f, 100.0f);
+  // sf::RectangleShape square(sf::Vector2f(LENGTH, LENGTH));
+  // square.setPosition(sf::Vector2f(top_left.x + LENGTH * 1, top_left.y + LENGTH * 1));
+  // square.setFillColor(sf::Color::Blue);
+
+  std::array<std::array<bool, WIDTH>, HEIGHT> grid = 
+    {{ 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}},
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}},
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}, 
+      {{0, 0, 0 ,0 ,0 ,0 ,0 ,0 ,0 ,0}}
+  }};
+  Board board(grid, sf::Color::White, sf::Vector2f(500.0f, 100.0f));
 
   auto window = sf::RenderWindow{ { 1920u, 1080u }, "Tetris" };
   window.setFramerateLimit(144);
@@ -29,24 +54,26 @@ auto main() -> int {
   sf::Time total_elapsed = sf::milliseconds(0);
   while (window.isOpen()) {
     total_elapsed += clock.restart();
-    if (total_elapsed >= sf::milliseconds(1'000)) {
-      for (auto& piece : pieces)
-        piece.moveDown();
+    if (total_elapsed >= sf::milliseconds(100)) {
+      board.moveDown();
       total_elapsed = sf::milliseconds(0);
     }
 
     for (auto event = sf::Event{}; window.pollEvent(event); ) {
       if (event.type == sf::Event::Closed) {
         window.close();
-      } else if (event.type == sf::Event::KeyPressed) {
-        for (auto& piece : pieces)
-          piece.handleKeyboardInput(event.key.code);
+      } 
+      else if (event.type == sf::Event::KeyPressed) {
+        // for (auto& piece : pieces)
+        //   piece.handleKeyboardInput(event.key.code);
+        board.handleKeyboardInput(event.key.code);
       }
     }
     window.clear();
-    for (const auto& piece : pieces) {
-      piece.draw(window);
-    }
+    board.draw(window);
+    // for (const auto& piece : pieces) {
+    //   piece.draw(window);
+    // }
     window.display();
   }
 }
